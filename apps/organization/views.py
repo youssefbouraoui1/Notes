3169c8073ea43756c01bdd3f8e6e914bab7de310
Youssef Bouraoui <bouraouiyoussef12@gmail.com>
2025-11-27 23:19:18 +0100

    added sending email in the orgnaization invitation logic

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -5,8 +5,9 @@ from .serializers import CreateOrganization, OrganizationList, SendInvitationReq
 from apps.users.customAuth import IsAuthenticatedCustom
 from rest_framework.decorators import api_view
 from .utils import generate_invite_token
-from django_project.constants import FRONTEND_BASE_URL
+from django_project.constants import FRONTEND_BASE_URL, ORGANIZATION_INVITATION_MAIL_SENDER
 from rest_framework.response import Response
+from django.core.mail import send_mail
 
 class ListCreateOrganization(generics.ListCreateAPIView):
     queryset = Organization.objects.all()
@@ -24,4 +25,13 @@ def invite_to_organization(request):
     
     token = generate_invite_token()
     invitation_url = f"{FRONTEND_BASE_URL}/organization/invite/{token}/"
+    user_mail = serializer.validated_data["invited_user"]
+    org = serializer.validated_data["organization"]
+    
+    send_mail(
+        subject=f"Invitation to {org.name} organization",
+        message = f"Please click on this link {invitation_url} to join the organization",
+        from_email= ORGANIZATION_INVITATION_MAIL_SENDER,
+        recipient_list=[user_mail]
+    )
     return Response({ "token": token, "url": invitation_url })
