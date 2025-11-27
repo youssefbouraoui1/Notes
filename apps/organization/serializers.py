a49ce78185837f4023b16f20aceac71f1f8a3961
Youssef Bouraoui <bouraouiyoussef12@gmail.com>
2025-11-27 22:35:49 +0100

    added organization creation and retrival

--- apps/organization/serializers.py
+++ apps/organization/serializers.py
@@ -1,3 +1,33 @@
 from rest_framework import serializers
+from .models import Organization
+from apps.users.models import Users
+from django.utils.text import slugify
 
 
+class CreateOrganization(serializers.ModelSerializer):
+    class Meta:
+        model = Organization
+        fields = ["name","owner_user_id","description"]
+        
+    def create(self, validated_data):
+        owner = Users.objects.get(id=validated_data.pop("owner_user_id"))
+
+        slug = slugify(validated_data["name"])
+        base_slug = slug
+        counter = 1
+        while Organization.objects.filter(slug=slug).exists():
+            slug = f"{base_slug}-{counter}"
+            counter += 1
+        org = Organization.objects.create(
+            slug=slug,
+            owner=owner,
+            **validated_data
+        )
+        
+        return org
+    
+
+class OrganizationList(serializers.ModelSerializer):
+    class Meta:
+        models = Organization
+        fields = ["name","slug","description","logo","owner"]
--- apps/organization/views.py
+++ apps/organization/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 from rest_framework import generics
+from .models import Organization
+from .serializers import CreateOrganization, OrganizationList
+from apps.users.customAuth import IsAuthenticatedCustom
 
-# Create your views here.
+class ListCreateOrganization(generics.ListCreateAPIView):
+    queryset = Organization.objects.all()
+    authentication_classes = [IsAuthenticatedCustom]
+    def get_serializer_class(self):
+        if self.request.method == "POST":
+            return CreateOrganization
+        return OrganizationList
